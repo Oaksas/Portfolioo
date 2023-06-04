884d5c7791cf50023fec3b66be8c4106c7248bbd
Oaksas <aman.getnet2@gmail.com>
2023-06-04 16:13:42 +0300

    Add nav section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,16 +9,34 @@ const Nav = () => {
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <div className="container mx-auto">
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50 ">
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between">
+          <Link
+            to="home"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between"
+          >
             <BiHomeAlt />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between">
+          <Link
+            to="about"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between"
+          >
             <BiUser />
           </Link>{" "}
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between">
+          <Link
+            to="services"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between"
+          >
             <BsClipboardData />
           </Link>{" "}
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between">
+          <Link
+            to="work"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between"
+          >
+            <BsBriefcase />
+          </Link>
+          <Link
+            to="contact"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-between"
+          >
             <BsBriefcase />
           </Link>{" "}
         </div>
