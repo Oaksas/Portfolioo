35c938ccb9c7e6a4ff612d3fa0c8f476c5d4ae9a
=oaksas <=>
2024-07-09 14:23:49 +0300

    Add cn and other config

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React, { useCallback } from "react";
+import React, { useCallback } from 'react'
 // components
-import Banner from "./components/Banner";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Services from "./components/Services";
-import Work from "./components/Work";
-import Contact from "./components/Contact";
-import { loadFull } from "tsparticles";
-import ParticlesContainer from "./components/particles";
-import AnimatedCursor from "react-animated-cursor";
-import { Toaster } from "react-hot-toast";
+import Banner from './components/Banner'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import About from './components/About'
+import Services from './components/Services'
+import Work from './components/Work'
+import Contact from './components/Contact'
+import { loadFull } from 'tsparticles'
+import ParticlesContainer from './components/particles'
+import AnimatedCursor from 'react-animated-cursor'
+import { Toaster } from 'react-hot-toast'
+import Skills from './components/Skills'
 
 const App = () => {
-  const customInit = useCallback(async (engine) => {
-    // this adds the bundle to tsParticles
-    await loadFull(engine);
-  });
+    const customInit = useCallback(async (engine) => {
+        // this adds the bundle to tsParticles
+        await loadFull(engine)
+    })
 
-  return (
-    <div className="bg-black overflow-hidden">
-      <Toaster position="top-center" reverseOrder={false} />
-      <AnimatedCursor />
-      <Header />
-      <Banner />
-      <Nav />
-      <About />
-      <Services />
-      <Work />
-      <Contact />
-      <ParticlesContainer />
-    </div>
-  );
-};
+    return (
+        <div className="bg-black overflow-hidden">
+            <Toaster position="top-center" reverseOrder={false} />
+            <AnimatedCursor />
+            <Header />
+            <Banner />
+            <Nav />
+            <About />
+            <Services />
+            <Work />
+            <Skills/>
+            <Contact />
+            <ParticlesContainer />
+        </div>
+    )
+}
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,61 @@
+const colors = require('tailwindcss/colors')
+const {
+    default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette')
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    fontFamily: {
-      primary: "Orbitron",
-      secondary: "Rajdhani",
-      tertiary: "Aldrich",
-      logo: "'Josefin Sans', sans-serif",
-    },
-    container: {
-      padding: {
-        DEFAULT: "15px",
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        fontFamily: {
+            primary: 'Orbitron',
+            secondary: 'Rajdhani',
+            tertiary: 'Aldrich',
+            logo: "'Josefin Sans', sans-serif",
+        },
+        container: {
+            padding: {
+                DEFAULT: '15px',
+            },
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '960px',
+            xl: '1200px',
+        },
+        extend: {
+            colors: {
+                primary: '#0a0a0a',
+                accent: '#B809C3',
+            },
+            backgroundImage: {
+                site: "url('./assets/site-bg.jpg')",
+                about: "url('./assets/banner/typing.svg')",
+                services: "url('./assets/Programming-bro.svg')",
+            },
+              animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
-    },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "960px",
-      xl: "1200px",
-    },
-    extend: {
-      colors: {
-        primary: "#0a0a0a",
-        accent: "#B809C3",
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
       },
-      backgroundImage: {
-        site: "url('./assets/site-bg.jpg')",
-        about: "url('./assets/banner/typing.svg')",
-        services: "url('./assets/Programming-bro.svg')",
-      },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [addVariablesForColors],
+  }
+
+  function addVariablesForColors({ addBase, theme }) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+   
+    addBase({
+      ":root": newVars,
+    });
+  }
